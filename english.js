5c1b84f067cbe3d88d2f1104378b6f12f66cc699
1one2two <ej04vm6@gmail.com>
2019-07-24 23:01:28 +0800

    add voice

--- english.js
+++ english.js
@@ -1,18 +1,11 @@
-let word = {
-    "afford": "給予",
-    "unfortunately": "不幸",
-    "waterprof": "防水",
-    "scratch": "抓",
-    "roof": "屋頂",
-    "tutor": "家教",
-    "assignment": "報告",
-    "material": "材料",
-    "recognize": "辨認",
-    "rub": "模插"
-}
+let word = { "exact": "確切", "thunder": "雷", "storm": "暴風雨", "breeze": "微風", "aruge": "爭論", "package": "包裹", "stairs": "樓梯", "tiptoe": "顛腳", "shovel": "鏟", "lie": "躺" }
 var cou = 0;
 var lis_q = [];
 var point = 0;
+var q_times = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+//var test = JSON.splice('./english.json');
+//console.log(test);
 
 window.onload = function () {
     pg = document.getElementById('po');
@@ -21,12 +14,16 @@ window.onload = function () {
     pg_q = document.getElementById('qe');
     pan_q = pg_q.getContext('2d');
     for (i in word) {
-        console.log(word[i]);
+        console.log(cou, i, word[i]);
         cou++;
     }
     draw_score();
-    new_q();
+    delay(1000).then(() => new_q());
 }
+
+$("#butts").click(function () {
+    alert("Your answer:" + $("#all_answer").val())
+});
 
 function get_key_value(ind) {
     var i = 0;
@@ -53,9 +50,11 @@ function new_q() {
             pan_q.fillStyle = "#FFFFFF";
             pan_q.fillRect(0, 0, pg_q.width, pg_q.height);
 
-            pan_q.fillStyle = "#49BED3";
+            pan_q.fillStyle = "#2AB6CF";
             pan_q.font = "30px Arial";
-            pan_q.fillText(get_key_value(lis_q[index]).e, 5, 20);
+            pan_q.textAlign = "center";
+            pan_q.fillText(get_key_value(lis_q[index]).e, 125, 25);
+            responsiveVoice.speak(get_key_value(lis_q[index]).e);
         }
         else
             document.getElementById('a' + index).value = get_key_value(lis_q[index]).c;
@@ -65,11 +64,14 @@ function new_q() {
 function re(id) {
     if (lis_q[id[1]] == lis_q[0]) {
         point++;
-        draw_score();
-        console.log(point);
+        q_times[lis_q[0]]++;
         new_q();
     }
-    else console.log("False");
+    else {
+        point = 0;
+        console.log("False");
+    }
+    draw_score();
 }
 
 function draw_score() {
@@ -78,6 +80,12 @@ function draw_score() {
 
     pan.fillStyle = "#000000";
     pan.font = "12px Arial";
+    pan.fillStyle = "";
     pan.fillText("Score:" + String(point), 5, 20);
-
 }
+
+var delay = function (s) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(resolve, s);
+    });
+};
