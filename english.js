4cb84a58adac2f0cbd21151f3bd3ae084faf95ce
1one2two <ej04vm6@gmail.com>
2019-08-07 22:48:17 +0800

    css change

--- english.js
+++ english.js
@@ -1,4 +1,4 @@
-let word = { "translate": "翻譯", "bulb": "燈泡", "enroll": "註冊", "community college": "社區大學", "sharpen": "加強", "course": "課程", "spilled": "打翻", "damp cloth": "抹布", "astronaut": "", "volunteers": "", "graduate": "畢業", "probably": "可能", "damage": "破壞", "affect": "", "quality": "品質", "generations": "世代", "resigned": "辭職", "algebra": "代數", "geometry": "幾何", "term": "學期" }
+let word = { "sharpen": "加強", "course": "課程", "spilled": "打翻", "damp cloth": "抹布", "astronaut": "太空人", "volunteers": "志願", "graduate": "畢業", "probably": "可能", "damage": "破壞", "affect": "影響", "quality": "品質", "generations": "世代", "resigned": "辭職", "algebra": "代數", "geometry": "幾何", "term": "學期" }
 var cou = 0;
 var lis_q = [];
 var point = 0;
@@ -6,6 +6,25 @@ var q_times = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 //var test = JSON.splice('./english.json');
 //console.log(test);
+
+document.onkeydown = function (e) {
+    switch (String(e.key)) {
+        case "1":
+            $('#a1').click();
+            break;
+        case "2":
+            $('#a2').click();
+            break;
+        case "3":
+            $('#a3').click();
+            break;
+        case "4":
+            $('#a4').click();
+            break;
+        default:
+            break;
+    }
+}
 
 window.onload = function () {
     pg = document.getElementById('po');
@@ -18,7 +37,7 @@ window.onload = function () {
         cou++;
     }
     draw_score();
-    delay(1000).then(() => new_q());
+    new_q(); //delay(1000).then(() => new_q());
 }
 
 $("#butts").click(function () {
@@ -47,14 +66,14 @@ function new_q() {
 
     lis_q.map((value, index) => {
         if (index == 0) {
-            pan_q.fillStyle = "#FFFFFF";
+            pan_q.fillStyle = "#9EE37D";
             pan_q.fillRect(0, 0, pg_q.width, pg_q.height);
 
-            pan_q.fillStyle = "#2AB6CF";
-            pan_q.font = "30px Arial";
+            pan_q.fillStyle = "black";
+            pan_q.font = "bold 38px Arial";
             pan_q.textAlign = "center";
-            pan_q.fillText(get_key_value(lis_q[index]).e, 125, 25);
-            responsiveVoice.speak(get_key_value(lis_q[index]).e);
+            pan_q.fillText(get_key_value(lis_q[index]).e, 310, 35);
+            //responsiveVoice.speak(get_key_value(lis_q[index]).e);
         }
         else
             document.getElementById('a' + index).value = get_key_value(lis_q[index]).c;
@@ -65,17 +84,27 @@ function re(id) {
     if (lis_q[id[1]] == lis_q[0]) {
         point++;
         q_times[lis_q[0]]++;
-        new_q();
+
+        $('#' + id).css('background-color', '#2AB6CF');
+        delay(180).then(() => {
+            new_q();
+            $('#' + id).css('background-color', '');
+        });
     }
     else {
         point = 0;
-        console.log("False");
+        $('#' + id).css('background-color', '#ff0000');
+        delay(180).then(() => {
+            new_q();
+            $('#' + id).css('background-color', '');
+        });
     }
     draw_score();
 }
 
 function draw_score() {
-    pan.fillStyle = "#FFFFFF";
+    //pan.globalAlpha= (Math.sin(0) + 1) / 2;
+    pan.fillStyle = "#9EE37D";
     pan.fillRect(0, 0, pg.width, pg.height);
 
     pan.fillStyle = "#000000";
